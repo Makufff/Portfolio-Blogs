3381fc8caa8cd5fe0c8acc0d924beb0a8860b825
makufff <Tanapatx.tyran@gmail.com>
2024-04-09 14:01:59 +0700

    fix all feat

--- src/lib/markdoc/frontmatter.schema.ts
+++ src/lib/markdoc/frontmatter.schema.ts
@@ -19,24 +19,13 @@ const baseSchema = z.object({
   }),
 });
 
-/*
-  Blog posts could be of two types —
-  1. The posts you write in markdown files in content/blog/*.md
-  2. External posts in other websites
-
-  That's why the frontmatter schema for blog posts is one of the two possible types.
-  If you don't want to link posts written in external websites, you could
-  simplify this to just use the markdown schema.
-*/
 export const blog = z.discriminatedUnion("external", [
-  // markdown
   baseSchema.extend({
     external: z.literal(false),
     description: z.optional(z.string()),
     ogImagePath: z.optional(z.string()),
     canonicalUrl: z.optional(z.string()),
   }),
-  // external link
   baseSchema.extend({
     external: z.literal(true),
     url: z.string({
