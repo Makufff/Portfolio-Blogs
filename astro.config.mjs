6173038a8d677b9a7b5cca767b05fd4a0ca24ee8
makufff <Tanapatx.tyran@gmail.com>
2024-04-09 16:50:59 +0700

    fix astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
-import vercel from "@astrojs/vercel/static";
-import astroRemark from "@astrojs/markdown-remark";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 const SERVER_PORT = 3000;
-// the url to access your blog during local development
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-// the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "0.0.0.0";
-// this is the astro command your npm script runs
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST_URL;
-// When you're building your site in local or in CI, you could just set your URL manually
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
+let BASE_URL = `http://localhost:${SERVER_PORT}`;
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   experimental: {
 		viewTransitions: true
@@ -54,14 +38,9 @@ export default defineConfig({
       gfm: false,
     }),
     partytown({
-      // Adds dataLayer.push as a forwarding-event.
       config: {
         forward: ["dataLayer.push"],
       },
     }),
   ],
-
-  // adapter: vercel({
-  //   analytics: true
-  // })
 });
